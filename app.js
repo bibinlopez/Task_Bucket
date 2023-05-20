92fb407e4b1b252882b3107ae195a4f511eda7fc
John <bibinlopez36@gmail.com>
2023-05-20 16:17:19 +0530

    notFound middleware

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/notFound')
 
 // middleware
 app.use(express.static('./public'))
 app.use(express.json());
 
 
-
 app.use('/api/v1/tasks', tasks);
+
+app.use(notFound)
 
 const port = 3000;
 
