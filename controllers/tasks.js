1ec03cab79b925d4eafdeccf2e52c5bb591442c5
John <bibinlopez36@gmail.com>
2023-05-20 10:02:12 +0530

    create task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,26 +1,34 @@
+const Task = require('../models/task');
 
-const getAllTasks = (req,res) => {
-   res.send('get all task')
-}
+const getAllTasks = (req, res) => {
+   res.send('get all task');
+};
 
 
-const createTask = (req, res) => {
-   res.json(req.body);
-}
+const createTask = async (req, res) => {
+   try {
+      const task = new Task(req.body);
+      const result = await task.save();
+      res.status(201).json({ result });
+   } catch (error) {
+      res.status(500).json({msg:error})
+   }
+};
+
 
 const getTask = (req, res) => {
-   res.json({id: req.params.id})
-} 
+   res.json({ id: req.params.id });
+};
 
 const updateTask = (req, res) => {
    res.send('update task');
-}
+};
 
 const deleteTask = (req, res) => {
    res.send('delete task');
-}
+};
 
 
 module.exports = {
    getAllTasks, createTask, getTask, updateTask, deleteTask
-}
+};
