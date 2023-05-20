98a09b266f2594ae2556be16558cd62b0d0ae7a1
John <bibinlopez36@gmail.com>
2023-05-20 11:11:19 +0530

    delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -25,10 +25,10 @@ const getTask = async (req, res) => {
    try {
       const { id: taskID } = req.params;
       const task = await Task.findOne({ _id: taskID });
-      if(!task){
-         return res.status(404).json({msg:`No task with id: ${taskID}`})
+      if (!task) {
+         return res.status(404).json({ msg: `No task with id: ${taskID}` });
       }
-      res.status(200).json({task});
+      res.status(200).json({ task });
    } catch (error) {
       res.status(500).json({ msg: error });
    }
@@ -38,8 +38,18 @@ const updateTask = (req, res) => {
    res.send('update task');
 };
 
-const deleteTask = (req, res) => {
-   res.send('delete task');
+const deleteTask = async (req, res) => {
+   try {
+      const { id: taskID } = req.params;
+      const task = await Task.findOneAndDelete({ _id: taskID });
+      if (!task) {
+         return res.status(404).json({ msg: `No task with Id: ${taskID}` });
+      }
+
+      res.status(200).json({msg: `Deleted Task Successfully with Id :${taskID}`});
+   } catch (error) {
+      res.status(500).json({ msg: error });
+   }
 };
 
 
