f15c673c3fab16b7badaf025708d7a137b291ad0
John <bibinlopez36@gmail.com>
2023-05-21 08:19:47 +0530

    threat removed

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
-// const connectionString = 'mongodb+srv://john:cool123@cluster0.v9szxtm.mongodb.net/myFirstDB' || process.env.CONNECTION_STRING;
-
 
 const connectDB = (url) => {
-   return mongoose.connect(url);
+   return mongoose.connect(url)
 };
 
 
